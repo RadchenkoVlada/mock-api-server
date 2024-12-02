13c78d770c7d75c06ecb7b68a04eeecd6b560495
Vlada Radchenko <ladon95@gmail.com>
2024-12-02 16:09:14 +0200

    Test for Get inventory has been done

--- tests/test_rest_api.py
+++ tests/test_rest_api.py
@@ -1,7 +1,16 @@
-# content of test_sample.py
-def func(x):
-    return x + 1
+import requests
+import json
 
+BASE_URL = "http://api-mock-server:80"
 
-def test_answer():
-    assert func(3) == 4
+def test_get_inventory_appliances():
+    """
+    Test inventory GET request for mock-api-server
+    """
+    with open("tests/test_data/GET_devices.json", 'r') as expected_data_file:
+        expected_data_json = json.load(expected_data_file)
+
+    rsp = requests.get(f"{BASE_URL}/inventory/devices")
+
+    assert rsp.status_code == 200
+    assert expected_data_json == rsp.json()
